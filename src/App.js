8fad8a9b9f0a806a3770e805da33520ecf032526
hyeoz <jenny970203@gmail.com>
2022-03-21 23:41:35 +0900

    feat. initial commit & chapter. 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// !MM 에서 사용하는 v5 사용!
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Chatpage from "./components/ChatPage/Chatpage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Chatpage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMuDzaxZp3CSifpUOxUbzXsReB1KUzUvM",
+  authDomain: "react-firebase-chat-24c0d.firebaseapp.com",
+  projectId: "react-firebase-chat-24c0d",
+  storageBucket: "react-firebase-chat-24c0d.appspot.com",
+  messagingSenderId: "412487588076",
+  appId: "1:412487588076:web:6d49b9991d3e303d76edc9",
+  measurementId: "G-55R0DWBL2Z",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
